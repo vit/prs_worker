91d6128344b56bd7189c5faa4f566b7bb71ace46
vit <none@none>
2016-08-27 02:53:45 +0400

    some of downloadable files have long file names without transliteration in subfolders

--- gen_archive/gen_archive.rb
+++ gen_archive/gen_archive.rb
@@ -7,7 +7,7 @@ $:.shift
 #work_dir = './work/'
 
 
-def write_files_to_archive lst, zipfile_name, types=nil
+def write_files_to_archive lst, zipfile_name, long_names=false, types=nil
 	Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
 		lst.each do |p|
 			cnt = p['_meta']['paper_cnt']
@@ -16,6 +16,7 @@ def write_files_to_archive lst, zipfile_name, types=nil
 			title_en = title['en']
 			title = title_en.to_s.length > 0 ? title_en.to_s : title_ru.to_s
 			title = title[0...50]
+			title_no_translit = title
 			title = Russian.translit(title)
 			title.gsub!(/[^0-9A-Za-z.\-]/, '_')
 			dir_name = ("%04d_" % cnt) + title
@@ -27,7 +28,8 @@ def write_files_to_archive lst, zipfile_name, types=nil
 					#@appl.conf.paper.get_paper_file cont_id, f['meta']['parent'], lang, cl
 					file = @appl.conf.paper.get_paper_file_by_id f[:_id]
 					file_ext = File.extname  f[:filename]
-					file_name = f[:class_code] + '_' + f[:_meta]['lang'] + file_ext
+				#	file_name = f[:class_code] + '_' + f[:_meta]['lang'] + file_ext
+					file_name = f[:class_code] + '_' + (long_names ? ("%04d_" % cnt)+title_no_translit+'_' : '') + f[:_meta]['lang'] + file_ext
 				#	file_name = f[:class_code] + '_' + f[:_meta]['lang']
 					#zipfile.get_output_stream(dir_name+'/'+file_name) { |os| os.write file }
 					zipfile.get_output_stream(dir_name+'/'+file_name) { |os| file.each{ |c| os.write c } }
@@ -67,17 +69,17 @@ end
 			#FileUtils.mv(zipfile_name, dst_dir)
 
 			zipfile_name = dir+'/'+'abstracts_'+cont_id.to_s+Time.new.strftime("_%Y_%m_%d_%H_%M_%S")+'.zip'
-			write_files_to_archive lst, zipfile_name, ['abstract']
+			write_files_to_archive lst, zipfile_name, true, ['abstract']
 			FileUtils.mkdir_p dst_dir
 			FileUtils.mv(zipfile_name, dst_dir)
 
 			zipfile_name = dir+'/'+'papers_'+cont_id.to_s+Time.new.strftime("_%Y_%m_%d_%H_%M_%S")+'.zip'
-			write_files_to_archive lst, zipfile_name, ['paper']
+			write_files_to_archive lst, zipfile_name, true, ['paper']
 			FileUtils.mkdir_p dst_dir
 			FileUtils.mv(zipfile_name, dst_dir)
 
 			zipfile_name = dir+'/'+'presentations_'+cont_id.to_s+Time.new.strftime("_%Y_%m_%d_%H_%M_%S")+'.zip'
-			write_files_to_archive lst, zipfile_name, ['presentation']
+			write_files_to_archive lst, zipfile_name, false, ['presentation']
 			FileUtils.mkdir_p dst_dir
 			FileUtils.mv(zipfile_name, dst_dir)
 
